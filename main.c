ae438ebf5fe30f17cb43cee0aae3674b4b4fa155
Isaac Duarte <iduarte@cs.stonybrook.edu>
2018-01-31 17:24:09 -0800

    Added method to load database from file

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 int addStudent(int, char*, char*, float, char*);
 int updateStudent(int, char*, char*, float, char*);
 int deleteStudent(int);
+int loadDatabase();
 
 typedef struct student_records{
 	struct student_records* next;
@@ -18,6 +19,8 @@ typedef struct student_records{
 } student_records;
 
 student_records* database;
+FILE* file;
+FILE* out;
 
 int main(int argc, char** argv) {
 	int vflag = 0;
@@ -26,15 +29,19 @@ int main(int argc, char** argv) {
 	char* major = NULL;
 	char* filename = NULL;
   	int c;
+  	out = stdout;
   	
   	database = (student_records*)malloc(sizeof(student_records));
-  	
-  	printf("%d\n", argc);
+  	database->prev = NULL;
+  	database->next = NULL;
+  	database->id = -1;
   	
   	if (argc <= 1){
-  		printf("NO QUERY PROVIDED\n");
+  		fprintf(out, "NO QUERY PROVIDED\n");
   		return 1;
   	}
+  	
+  	char* fileIn = *(argv+1);
   	
   	while((c = getopt(argc, argv, "vi:f:m:o:")) != -1){
 		switch(c){
@@ -54,19 +61,54 @@ int main(int argc, char** argv) {
 				filename = optarg;
 				break;
 			default:
-				printf("FAILED TO PARSE FILE\n");
+				fprintf(out, "FAILED TO PARSE FILE\n");
 				return 1;
 		}
 	}
+	
+	file = fopen(fileIn, "r+");
+	loadDatabase();
   	return 0;
+}
+
+int loadDatabase(){
+	char* command;
+	char* fName;
+	char* lName;
+	char* major;
+	int id;
+	float gpa;
+	do{
+		fscanf(file, "%s", command);
+		switch(*command){
+			case 'A':
+				fscanf(file, "%d %s %s %f %s ", &id, fName, lName, &gpa, major);
+				addStudent(id, fName, lName, gpa, major);
+				break;
+			case 'D':
+				fscanf(file, "%d ", &id);
+				deleteStudent(id);
+				break;
+			case 'U':
+				fscanf(file, "%d %s %s %f %s ", &id, fName, lName, &gpa, major);
+				updateStudent(id, fName, lName, gpa, major);
+				break;
+			default:
+				return -1;
+		}
+	} while (0);
 }
 
 int deleteStudent(int id){
 	student_records* cursor = database;
 	while(cursor != NULL){
 		if (cursor->id == id){
-			cursor->prev->next = cursor->next;
-			cursor->next->prev = cursor->prev;
+			if (cursor->prev != NULL){
+				cursor->prev->next = cursor->next;
+			}
+			if (cursor->next != NULL){
+				cursor->next->prev = cursor->prev;
+			}
 			free(cursor);
 			return 0;
 		} else {
@@ -83,6 +125,8 @@ int addStudent(int id, char* firstName, char* lastName, float gpa, char* major){
 			return -1;
 		} else if (cursor->id > id){
 			student_records* add;
+			add = (student_records*)malloc(sizeof(student_records));
+			
 			add->next = cursor;
 			add->prev = cursor->prev;
 			add->id = id;
@@ -90,12 +134,38 @@ int addStudent(int id, char* firstName, char* lastName, float gpa, char* major){
 			add->last_name = lastName;
 			add->gpa = gpa;
 			add->major = major;
-			cursor->prev->next = add;
+			
+			if (cursor->prev != NULL){
+				cursor->prev->next = add;
+			} else{
+				database = add;
+			}
 			cursor->prev = add;
 			return 0;
-		} else {
-			cursor = cursor->next;
+		} else if (cursor->next == NULL){
+			if (cursor->id == -1){
+				cursor->id = id;
+				cursor->first_name = firstName;
+				cursor->last_name = lastName;
+				cursor->gpa = gpa;
+				cursor->major = major;
+			} else {
+				student_records* add;
+				add = (student_records*)malloc(sizeof(student_records));
+			
+				add->next = NULL;
+				add->prev = cursor;
+				add->id = id;
+				add->first_name = firstName;
+				add->last_name = lastName;
+				add->gpa = gpa;
+				add->major = major;
+				
+				cursor->next = add;
+			}
+			return 0;
 		}
+		cursor = cursor->next;
 	}
 }
 
