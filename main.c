0bb2b9f7dcb26ca13b20849a7d71612b984098bc
Isaac Duarte <iduarte@cs.stonybrook.edu>
2018-02-01 10:39:27 -0800

    Properly load database from file!

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int addStudent(int, char*, char*, float, char*);
 int updateStudent(int, char*, char*, float, char*);
 int deleteStudent(int);
+int stringCopy(char*, char**);
 int loadDatabase();
 
 typedef struct student_records{
@@ -68,19 +70,28 @@ int main(int argc, char** argv) {
 	
 	file = fopen(fileIn, "r+");
 	loadDatabase();
-  	return 0;
+	while(database != NULL){
+		printf("%d:%s %s %.2f %s->", database->id, database->first_name, database->last_name, database->gpa, database->major);
+		database = database->next;
+	}
+	printf("\n");
+	fclose(file);
+  return 0;
 }
 
 int loadDatabase(){
-	char* command;
-	char* fName;
-	char* lName;
-	char* major;
+	char command[255];
+	char fName[255];
+	char lName[255];
+	char major[255];
 	int id;
 	float gpa;
+	int c;
 	do{
-		fscanf(file, "%s", command);
-		switch(*command){
+		c = fscanf(file, "%s", command);
+		if (c == EOF) return 0;
+
+		switch(toupper(*command)){
 			case 'A':
 				fscanf(file, "%d %s %s %f %s ", &id, fName, lName, &gpa, major);
 				addStudent(id, fName, lName, gpa, major);
@@ -96,7 +107,7 @@ int loadDatabase(){
 			default:
 				return -1;
 		}
-	} while (0);
+	} while (1);
 }
 
 int deleteStudent(int id){
@@ -109,6 +120,9 @@ int deleteStudent(int id){
 			if (cursor->next != NULL){
 				cursor->next->prev = cursor->prev;
 			}
+			free(cursor->first_name);
+			free(cursor->last_name);
+			free(cursor->major);
 			free(cursor);
 			return 0;
 		} else {
@@ -123,6 +137,28 @@ int addStudent(int id, char* firstName, char* lastName, float gpa, char* major){
 	while(cursor != NULL){
 		if (cursor->id == id){
 			return -1;
+		} else if (cursor->next == NULL && cursor->id < id){
+			if (cursor->id == -1){
+				cursor->id = id;
+				stringCopy(firstName, &(cursor->first_name));
+				stringCopy(lastName, &(cursor->last_name));
+				cursor->gpa = gpa;
+				stringCopy(major, &(cursor->major));
+			} else {
+				student_records* add;
+				add = (student_records*)malloc(sizeof(student_records));
+				printf("%p %p\n", add->first_name, firstName);
+				add->next = NULL;
+				add->prev = cursor;
+				add->id = id;
+				stringCopy(firstName, &(add->first_name));
+				stringCopy(lastName, &(add->last_name));
+				add->gpa = gpa;
+				stringCopy(major, &(add->major));
+				
+				cursor->next = add;
+			}
+			return 0;
 		} else if (cursor->id > id){
 			student_records* add;
 			add = (student_records*)malloc(sizeof(student_records));
@@ -130,10 +166,10 @@ int addStudent(int id, char* firstName, char* lastName, float gpa, char* major){
 			add->next = cursor;
 			add->prev = cursor->prev;
 			add->id = id;
-			add->first_name = firstName;
-			add->last_name = lastName;
+			stringCopy(firstName, &(add->first_name));
+			stringCopy(lastName, &(add->last_name));
 			add->gpa = gpa;
-			add->major = major;
+			stringCopy(major, &(add->major));
 			
 			if (cursor->prev != NULL){
 				cursor->prev->next = add;
@@ -141,28 +177,6 @@ int addStudent(int id, char* firstName, char* lastName, float gpa, char* major){
 				database = add;
 			}
 			cursor->prev = add;
-			return 0;
-		} else if (cursor->next == NULL){
-			if (cursor->id == -1){
-				cursor->id = id;
-				cursor->first_name = firstName;
-				cursor->last_name = lastName;
-				cursor->gpa = gpa;
-				cursor->major = major;
-			} else {
-				student_records* add;
-				add = (student_records*)malloc(sizeof(student_records));
-			
-				add->next = NULL;
-				add->prev = cursor;
-				add->id = id;
-				add->first_name = firstName;
-				add->last_name = lastName;
-				add->gpa = gpa;
-				add->major = major;
-				
-				cursor->next = add;
-			}
 			return 0;
 		}
 		cursor = cursor->next;
@@ -173,17 +187,26 @@ int updateStudent(int id, char* firstName, char* lastName, float gpa, char* majo
 	student_records* cursor = database;
 	while(cursor != NULL){
 		if (cursor->id == id){
-			free(cursor->first_name);
-			free(cursor->last_name);
-			free(cursor->major);
-			cursor->first_name = firstName;
-			cursor->last_name = lastName;
+			stringCopy(firstName, &(cursor->first_name));
+			stringCopy(lastName, &(cursor->last_name));
 			cursor->gpa = gpa;
-			cursor->major = major;
+			stringCopy(major, &(cursor->major));
 			return 0;
 		} else {
 			cursor = cursor->next;
 		}
 	}
 	return -1;
+}
+
+int stringCopy(char* source, char** dest){
+	int i;
+	int len = 0;
+	for(i = 0; *(source+i) != '\0'; i++)
+		len++;
+	char* temp = (char*)malloc((len+1) * sizeof(char));
+	for(i = 0; i <= len; i++)
+		*(temp+i) = *(source+i);
+	(*dest) = temp;
+	return 0;
 }
